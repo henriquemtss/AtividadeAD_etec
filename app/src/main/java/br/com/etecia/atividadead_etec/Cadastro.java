b3b0627cbc66a8e0b62f884de22f433e2fe6bec2
Henrique2410 <henriquegomesdematos08@gmail.com>
2022-04-06 15:26:21 -0300

    atividade finalizada

---
+++ app/src/main/java/br/com/etecia/atividadead_etec/Cadastro.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atividadead_etec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
